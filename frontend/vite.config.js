1c1a314956c4424dfefc8177c068b7c9238dba85
Loja Virtual Estock de Kasa <anakimkgb@gmail.com>
2025-10-10 21:37:08 -0500

    tttt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/site/' // 👈 importante: deve ser igual ao nome do repositório ou subpasta do GitHub Pages
+  base: '/https://github.com/lojavirtual-stk/site/' // 👈 importante: deve ser igual ao nome do repositório ou subpasta do GitHub Pages
 })
